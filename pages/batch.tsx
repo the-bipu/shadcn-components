331593193e87f4772caa428871cb2d3c987c8f67
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-12-10 23:54:34 +0530

    Create batch.tsx

---
+++ pages/batch.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const BatchPage = () => {
+    return (
+        <div className='flex flex-col items-center justify-center w-full h-screen'>
+            <div className='md:w-10/12 w-full h-full flex flex-col gap-4 my-10'>
+                <div>
+                    <Button variant={'default'}>Default</Button>
+                </div>
+                <div>
+                    <Button variant={'destructive'}>Destructive</Button>
+                </div>
+                <div>
+                    <Button variant={'bootstrap'}>Bootstrap</Button>
+                </div>
+                <div>
+                    <Button variant={'success'}>Success</Button>
+                </div>
+                <div>
+                    <Button variant={'warning'}>Warning</Button>
+                </div>
+                <div>
+                    <Button variant={'outline'}>Outline</Button>
+                </div>
+                <div>
+                    <Button variant={'secondary'}>Secondary</Button>
+                </div>
+                <div>
+                    <Button variant={'ghost'}>Ghost</Button>
+                </div>
+                <div>
+                    <Button variant={'link'}>Link</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BatchPage
