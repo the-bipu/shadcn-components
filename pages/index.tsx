c61f2598e40678d89daaaedff1d22c0011c93b81
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-12-13 22:10:52 +0530

    added new components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
+import Navbar from '@/components/common/navbar'
 import React from 'react'
 
 const IndexPage = () => {
     return (
-        <div>IndexPage</div>
+        <div className='w-full min-h-screen flex flex-col items-center'>
+
+            <Navbar />
+
+            <div className='w-10/12 h-full flex flex-col'>
+
+
+
+            </div>
+        </div>
     )
 }
 
